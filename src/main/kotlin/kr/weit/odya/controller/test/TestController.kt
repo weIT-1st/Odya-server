5a2a2f33afb6a0118c5c2221698800580bde250d
Chae <oumuamua121@gmail.com>
2023-05-23 23:27:31 +0900

    ODYA-7 리뷰 반영

--- src/main/kotlin/kr/weit/odya/controller/test/TestController.kt
+++ src/main/kotlin/kr/weit/odya/controller/test/TestController.kt
@@ -1,13 +1,15 @@
-package kr.weit.odya.controller
+package kr.weit.odya.controller.test
 
+import kr.weit.odya.controller.test.response.TestRespones
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class TestController {
+    // TODO: 이 API는 클라이언트와의 연동 테스트를 위한 임시 API이므로, 추후 삭제해야 함
     @PostMapping("/test")
-    fun test(@RequestBody name: String): Pair<Long, String> {
+    fun test(@RequestBody name: String): TestRespones {
         val trimedName = name.trim()
         if (trimedName.isEmpty()) {
             throw IllegalArgumentException("이름이 비어있으면 안됨")
@@ -15,6 +17,6 @@ class TestController {
         if (trimedName == "김한빈") {
             throw IllegalArgumentException("김한빈은 안됨")
         }
-        return Pair(trimedName.hashCode().toLong(), trimedName)
+        return TestRespones(trimedName.hashCode(), trimedName)
     }
 }
