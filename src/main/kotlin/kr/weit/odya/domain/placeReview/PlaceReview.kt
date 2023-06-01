60f8bd7ee312193a595a9273ad953a238c762333
Chae <oumuamua121@gmail.com>
2023-06-01 19:14:49 +0900

    ODYA-19 Add BaseTimeEntity

--- src/main/kotlin/kr/weit/odya/domain/placeReview/PlaceReview.kt
+++ src/main/kotlin/kr/weit/odya/domain/placeReview/PlaceReview.kt
@@ -6,9 +6,8 @@ import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
+import kr.weit.odya.domain.BaseTimeEntity
 import kr.weit.odya.domain.user.User
-import org.springframework.data.annotation.CreatedDate
-import java.time.LocalDateTime
 
 @Entity
 @Table(name = "place_review")
@@ -26,9 +25,5 @@ class PlaceReview(
     val starRating: Int,
 
     @Column(length = 300, nullable = false, updatable = false)
-    val comment: String,
-
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    val createdDate: LocalDateTime = LocalDateTime.now(),
-)
+    val comment: String
+) : BaseTimeEntity()
--- src/test/resources/sql/data-h2.sql
+++ src/test/resources/sql/data-h2.sql
@@ -3,4 +3,4 @@ values (1, 'kakao_test12314123', 'test@test.com', 'testNickname', '010-1111-1111
         to_date('1990-01-01', 'YYYY-MM-DD'), 'defualt_profile.png', 'KAKAO', null, sysdate);
 
 insert into PLACE_REVIEW
-values ('ChIJgUbEo8cfqokR5lP9_Wh_DaM', 1, 10, '나이지리아 너무 좋네요.', sysdate);
+values ('ChIJgUbEo8cfqokR5lP9_Wh_DaM', 1, 10, '나이지리아 너무 좋네요.', sysdate, sysdate);
