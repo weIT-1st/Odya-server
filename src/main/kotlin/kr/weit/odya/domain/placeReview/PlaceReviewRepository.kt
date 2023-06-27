6113bc1af2149576cafc009d966cc6c0383f59f2
YC <gks54410@gmail.com>
2023-06-27 15:42:12 +0900

    ODYA-60 인덴트 수정

--- src/main/kotlin/kr/weit/odya/domain/placeReview/PlaceReviewRepository.kt
+++ src/main/kotlin/kr/weit/odya/domain/placeReview/PlaceReviewRepository.kt
@@ -11,16 +11,16 @@ fun PlaceReviewRepository.getByPlaceReviewId(id: Long): PlaceReview =
     findByIdOrNull(id) ?: throw NoSuchElementException("$id: 존재하지 않는 장소 리뷰입니다.")
 
 fun PlaceReviewRepository.getListByPlaceIdInitial(placeId: String, page: Pageable): Slice<PlaceReview> =
-        findByPlaceId(placeId, page)
+    findByPlaceId(placeId, page)
 
 fun PlaceReviewRepository.getListByPlaceIdStartId(placeId: String, startId: Long, page: Pageable): Slice<PlaceReview> =
-        findByPlaceIdAndIdLessThan(placeId, startId, page)
+    findByPlaceIdAndIdLessThan(placeId, startId, page)
 
 fun PlaceReviewRepository.getListByUserInitial(user: User, page: Pageable): Slice<PlaceReview> =
-        findByUser(user, page)
+    findByUser(user, page)
 
 fun PlaceReviewRepository.getListByUserStartId(user: User, startId: Long, page: Pageable): Slice<PlaceReview> =
-        findByUserAndIdLessThan(user, startId, page)
+    findByUserAndIdLessThan(user, startId, page)
 
 @Repository
 interface PlaceReviewRepository : JpaRepository<PlaceReview, Long> {
