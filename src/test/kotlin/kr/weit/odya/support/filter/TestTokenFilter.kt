b866471656a56666778f0446cdb545d7dbd67eee
YC <gks54410@gmail.com>
2023-07-18 18:44:09 +0900

    ODYA-66 테스트 코드 수정
    - 에러 상황의 response-fields snippet 정의 제거

--- src/test/kotlin/kr/weit/odya/support/filter/TestTokenFilter.kt
+++ src/test/kotlin/kr/weit/odya/support/filter/TestTokenFilter.kt
@@ -1,11 +1,19 @@
 package kr.weit.odya.support.filter
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import kr.weit.odya.service.dto.ErrorResponse
+import kr.weit.odya.support.SOMETHING_ERROR_MESSAGE
 import kr.weit.odya.support.TEST_BEARER_ID_TOKEN
+import kr.weit.odya.support.TEST_BEARER_INVALID_ID_TOKEN
+import kr.weit.odya.support.TEST_BEARER_NOT_EXIST_USER_ID_TOKEN
+import kr.weit.odya.support.exception.ErrorCode
 import kr.weit.odya.support.userAuthentication
 import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
 
@@ -15,10 +23,24 @@ class TestTokenFilter : OncePerRequestFilter() {
         response: HttpServletResponse,
         filterChain: FilterChain,
     ) {
-        val loginToken = request.getHeader(HttpHeaders.AUTHORIZATION)
-        if (loginToken == TEST_BEARER_ID_TOKEN) {
-            SecurityContextHolder.getContext().authentication = userAuthentication()
+        when (request.getHeader(HttpHeaders.AUTHORIZATION)) {
+            null -> {
+                filterChain.doFilter(request, response)
+            }
+
+            TEST_BEARER_ID_TOKEN -> {
+                SecurityContextHolder.getContext().authentication = userAuthentication()
+                filterChain.doFilter(request, response)
+            }
+
+            TEST_BEARER_NOT_EXIST_USER_ID_TOKEN, TEST_BEARER_INVALID_ID_TOKEN -> {
+                response.status = HttpStatus.UNAUTHORIZED.value()
+                response.contentType = MediaType.APPLICATION_JSON_VALUE
+                jacksonObjectMapper().writeValue(
+                    response.outputStream,
+                    ErrorResponse.of(ErrorCode.INVALID_FIREBASE_ID_TOKEN, SOMETHING_ERROR_MESSAGE),
+                )
+            }
         }
-        filterChain.doFilter(request, response)
     }
 }
