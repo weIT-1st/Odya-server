c978d3895c2372486dad4100e6cee9b282981217
Chae <oumuamua121@gmail.com>
2023-07-25 00:20:26 +0900

    ODYA-78 리뷰 적용

--- src/test/kotlin/kr/weit/odya/domain/favoritePlace/FavoritePlaceRepositoryTest.kt
+++ src/test/kotlin/kr/weit/odya/domain/favoritePlace/FavoritePlaceRepositoryTest.kt
@@ -1,4 +1,4 @@
-package kr.weit.odya.domain.interestPlace
+package kr.weit.odya.domain.favoritePlace
 
 import io.kotest.core.spec.style.ExpectSpec
 import io.kotest.extensions.spring.SpringTestExtension
@@ -6,15 +6,15 @@ import io.kotest.extensions.spring.SpringTestLifecycleMode
 import io.kotest.matchers.shouldBe
 import kr.weit.odya.domain.user.User
 import kr.weit.odya.domain.user.UserRepository
-import kr.weit.odya.support.TEST_INTEREST_PLACE_ID
+import kr.weit.odya.support.TEST_FAVORITE_PLACE_ID
 import kr.weit.odya.support.TEST_PLACE_ID
-import kr.weit.odya.support.createInterestPlace
+import kr.weit.odya.support.createFavoritePlace
 import kr.weit.odya.support.createUser
 import kr.weit.odya.support.test.BaseTests.RepositoryTest
 
 @RepositoryTest
-class InterestPlaceRepositoryTest(
-    private val interestPlaceRepository: InterestPlaceRepository,
+class FavoritePlaceRepositoryTest(
+    private val favoritePlaceRepository: FavoritePlaceRepository,
     private val userRepository: UserRepository,
 ) : ExpectSpec(
     {
@@ -22,21 +22,21 @@ class InterestPlaceRepositoryTest(
         lateinit var user: User
         beforeEach {
             user = userRepository.save(createUser())
-            interestPlaceRepository.save(createInterestPlace(user))
+            favoritePlaceRepository.save(createFavoritePlace(user))
         }
 
         context("관심 장소 조회") {
 
             expect("user와 placeId가 일치하는 관심 장소를 조회한다") {
-                val result = interestPlaceRepository.getByInterestPlaceId(TEST_INTEREST_PLACE_ID)
-                result.id shouldBe TEST_INTEREST_PLACE_ID
+                val result = favoritePlaceRepository.getByFavoritePlaceId(TEST_FAVORITE_PLACE_ID)
+                result.id shouldBe TEST_FAVORITE_PLACE_ID
             }
         }
 
         context("관심 장소 여부 확인") {
 
             expect("USER_ID와 PLACE_ID이 일치하는 장소 리뷰 여부를 확인한다") {
-                val result = interestPlaceRepository.existsByUserIdAndPlaceId(user.id, TEST_PLACE_ID)
+                val result = favoritePlaceRepository.existsByUserIdAndPlaceId(user.id, TEST_PLACE_ID)
                 result shouldBe true
             }
         }
---
+++ src/test/kotlin/kr/weit/odya/support/FavoritePlaceFixtures.kt
@@ -0,0 +1,12 @@
+package kr.weit.odya.support
+
+import kr.weit.odya.domain.favoritePlace.FavoritePlace
+import kr.weit.odya.domain.user.User
+import kr.weit.odya.service.dto.FavoritePlaceRequest
+
+const val TEST_FAVORITE_PLACE_ID = 1L
+const val TEST_INVALID_FAVORITE_PLACE_ID = -1L
+const val TEST_EXIST_FAVORITE_PLACE_ID = 10L
+fun createFavoritePlace(user: User = createUser()) = FavoritePlace(TEST_FAVORITE_PLACE_ID, TEST_PLACE_ID, user)
+
+fun createFavoritePlaceRequest() = FavoritePlaceRequest(TEST_PLACE_ID)
