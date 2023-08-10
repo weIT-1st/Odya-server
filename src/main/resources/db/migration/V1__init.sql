722b16506e8d264433379bd8cc85a3b7e446ff59
Chae <oumuamua121@gmail.com>
2023-08-11 00:31:25 +0900

    ODYA-104 sql문 수정

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -9,6 +9,7 @@ CREATE TABLE users
     birthday      DATE           NOT NULL,
     profile_name  VARCHAR2(255)  NOT NULL,
     social_type   VARCHAR2(10)   NOT NULL,
+    withdraw_date DATE           NULL,
     created_date  DATE           NOT NULL,
     PRIMARY KEY (id)
 );
