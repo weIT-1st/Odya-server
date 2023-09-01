342d0b85a457744663dda9e739059fbb37b55de3
konempty <jameswebb@naver.com>
2023-09-01 22:11:31 +0900

    ODYA-136 깨지는 테스트 수정

--- src/test/kotlin/kr/weit/odya/domain/traveljournal/TravelJournalRepositoryTest.kt
+++ src/test/kotlin/kr/weit/odya/domain/traveljournal/TravelJournalRepositoryTest.kt
@@ -5,8 +5,8 @@ import io.kotest.matchers.shouldBe
 import kr.weit.odya.domain.contentimage.ContentImageRepository
 import kr.weit.odya.domain.user.User
 import kr.weit.odya.domain.user.UserRepository
-import kr.weit.odya.support.TEST_USER_ID
 import kr.weit.odya.support.TEST_TRAVEL_JOURNAL_ID
+import kr.weit.odya.support.TEST_USER_ID
 import kr.weit.odya.support.createContentImage
 import kr.weit.odya.support.createOtherUser
 import kr.weit.odya.support.createTravelCompanionById
@@ -42,17 +42,18 @@ class TravelJournalRepositoryTest(
                 ),
             )
         }
-        context("여행일지 사용자 Id로 조회") {
-            expect("유저 ID와 일치하는 여행기록을 조회한다.") {
-                val result = travelJournalRepository.getByUserId(TEST_USER_ID)
-                result.size shouldBe 1
-            }
-        }
 
         context("여행 일지 조회") {
             expect("여행 일지 ID와 일치하는 여행 일지를 조회한다.") {
                 val result = travelJournalRepository.getByTravelJournalId(TEST_TRAVEL_JOURNAL_ID)
                 result.id shouldBe TEST_TRAVEL_JOURNAL_ID
+            }
+        }
+
+        context("여행일지 사용자 Id로 조회") {
+            expect("유저 ID와 일치하는 여행기록을 조회한다.") {
+                val result = travelJournalRepository.getByUserId(TEST_USER_ID)
+                result.size shouldBe 1
             }
         }
     },
