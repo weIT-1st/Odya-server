f06640aac7c670d437a2322d42c2384ad79dcc5e
YC <gks54410@gmail.com>
2023-09-05 11:26:01 +0900

    ODYA-125 디버깅을 위한 값 제거

--- src/test/kotlin/kr/weit/odya/domain/user/UserRepositoryTest.kt
+++ src/test/kotlin/kr/weit/odya/domain/user/UserRepositoryTest.kt
@@ -25,20 +25,16 @@ class UserRepositoryTest(
 
         context("사용자 조회") {
             expect("USERNAME이 일치하는 사용자를 조회한다") {
-                println("asdadasdadasdasdasd : " + userRepository.findAll().size)
-
                 val result = userRepository.getByUsername(user.username)
                 result.username shouldBe user.username
             }
 
             expect("USER_ID가 일치하는 사용자를 조회한다") {
-                println("asdadasdadasdasdasd : " + userRepository.findAll().size)
                 val result = userRepository.getByUserId(user.id)
                 result.username shouldBe user.username
             }
 
             expect("USER_ID가 일치하는 사용자를 프로필과 함께 조회한다") {
-                println("asdadasdadasdasdasd : " + userRepository.findAll().size)
                 val result = userRepository.getByUserIdWithProfile(user.id)
                 result.profile.profileName shouldBe TEST_DEFAULT_PROFILE_PNG
             }
@@ -46,7 +42,6 @@ class UserRepositoryTest(
 
         context("사용자 목록 조회") {
             expect("사용자 ID 목록에 해당되는 모든 사용자를 조회한다") {
-                println("asdadasdadasdasdasd : " + userRepository.findAll().size)
                 val userIds = listOf(user.id, otherUser.id)
                 val result = userRepository.getByUserIds(userIds)
                 result.size shouldBe 2
@@ -55,31 +50,26 @@ class UserRepositoryTest(
 
         context("사용자 여부 확인") {
             expect("USERNAME이 일치하는 사용자 여부를 확인한다") {
-                println("asdadasdadasdasdasd : " + userRepository.findAll().size)
                 val result = userRepository.existsByUsername(user.username)
                 result shouldBe true
             }
 
             expect("닉네임이 일치하는 사용자 여부를 확인한다") {
-                println("asdadasdadasdasdasd : " + userRepository.findAll().size)
                 val result = userRepository.existsByNickname(user.nickname)
                 result shouldBe true
             }
 
             expect("이메일이 일치하는 사용자 여부를 확인한다") {
-                println("asdadasdadasdasdasd : " + userRepository.findAll().size)
                 val result = userRepository.existsByEmail(TEST_EMAIL)
                 result shouldBe true
             }
 
             expect("전화번호가 일치하는 사용자 여부를 확인한다") {
-                println("asdadasdadasdasdasd : " + userRepository.findAll().size)
                 val result = userRepository.existsByPhoneNumber(TEST_PHONE_NUMBER)
                 result shouldBe true
             }
 
             expect("사용자 ID 목록에 해당하는 모든 사용자 여부를 확인한다") {
-                println("asdadasdadasdasdasd : " + userRepository.findAll().size)
                 val userIds = listOf(user.id, otherUser.id)
                 val result = userRepository.existsAllByUserIds(userIds, userIds.size)
                 result shouldBe true
@@ -88,21 +78,18 @@ class UserRepositoryTest(
 
         context("사용자 id list 검색") {
             expect("사용자 id list에 해당하는 사용자를 조회한다") {
-                println("asdadasdadasdasdasd : " + userRepository.findAll().size)
                 val userIds = listOf(user.id, otherUser.id)
                 val result = userRepository.getByUserIds(userIds, TEST_DEFAULT_SIZE, null)
                 result.size shouldBe 2
             }
 
             expect("사용자 id list에 해당하는 사용자를 size만큼 조회한다") {
-                println("asdadasdadasdasdasd : " + userRepository.findAll().size)
                 val userIds = listOf(user.id, otherUser.id)
                 val result = userRepository.getByUserIds(userIds, 1, null)
                 result.size shouldBe 1
             }
 
             expect("사용자 id list에 해당하는 lastId보다 작은 유저를 조회한다") {
-                println("asdadasdadasdasdasd : " + userRepository.findAll().size)
                 val userIds = listOf(user.id, otherUser.id)
                 var result = userRepository.getByUserIds(userIds, TEST_DEFAULT_SIZE, otherUser.id)
                 result.size shouldBe 1
