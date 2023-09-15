bc515c3fe1ef3bc0e624ec5140dc14e6c227042a
YC <gks54410@gmail.com>
2023-09-15 21:19:35 +0900

    ODYA-144 여행 일지 추가 기능 구현 테스트

--- src/test/kotlin/kr/weit/odya/domain/traveljournal/TravelJournalRepositoryTest.kt
+++ src/test/kotlin/kr/weit/odya/domain/traveljournal/TravelJournalRepositoryTest.kt
@@ -2,6 +2,8 @@ package kr.weit.odya.domain.traveljournal
 
 import io.kotest.core.spec.style.ExpectSpec
 import io.kotest.matchers.shouldBe
+import kr.weit.odya.domain.follow.Follow
+import kr.weit.odya.domain.follow.FollowRepository
 import kr.weit.odya.domain.user.User
 import kr.weit.odya.domain.user.UserRepository
 import kr.weit.odya.support.TEST_TRAVEL_JOURNAL_TITLE
@@ -17,17 +19,22 @@ import kr.weit.odya.support.test.BaseTests.RepositoryTest
 @RepositoryTest
 class TravelJournalRepositoryTest(
     private val userRepository: UserRepository,
+    private val followRepository: FollowRepository,
     private val travelJournalRepository: TravelJournalRepository,
 ) : ExpectSpec(
     {
         lateinit var user: User
         lateinit var otherUser: User
         lateinit var travelJournal: TravelJournal
+        lateinit var otherTravelJournal: TravelJournal
+        lateinit var friendTravelJournal: TravelJournal
         beforeEach {
             user = userRepository.save(createUser())
             otherUser = userRepository.save(createOtherUser())
-            travelJournal = travelJournalRepository.save(
+            followRepository.save(Follow(user, otherUser))
+            val travelJournals = listOf(
                 createTravelJournal(
+                    id = 1L,
                     user = user,
                     travelCompanions = listOf(createTravelCompanionById(user = otherUser)),
                     travelJournalContents = listOf(
@@ -38,7 +45,37 @@ class TravelJournalRepositoryTest(
                         ),
                     ),
                 ),
+                createTravelJournal(
+                    id = 2L,
+                    user = user,
+                    title = "otherTravelJournal",
+                    travelCompanions = listOf(createTravelCompanionById(user = otherUser)),
+                    travelJournalContents = listOf(
+                        createTravelJournalContent(
+                            travelJournalContentImages = listOf(
+                                createTravelJournalContentImage(contentImage = createContentImage(user = user)),
+                            ),
+                        ),
+                    ),
+                ),
+                createTravelJournal(
+                    id = 3L,
+                    user = otherUser,
+                    title = "friendTravelJournal",
+                    travelCompanions = listOf(createTravelCompanionById(user = user)),
+                    travelJournalContents = listOf(
+                        createTravelJournalContent(
+                            travelJournalContentImages = listOf(
+                                createTravelJournalContentImage(contentImage = createContentImage(user = otherUser)),
+                            ),
+                        ),
+                    ),
+                ),
             )
+            val saveTravelJournals = travelJournalRepository.saveAll(travelJournals)
+            travelJournal = saveTravelJournals[0]
+            otherTravelJournal = saveTravelJournals[1]
+            friendTravelJournal = saveTravelJournals[2]
         }
 
         context("여행 일지 조회") {
@@ -48,10 +85,44 @@ class TravelJournalRepositoryTest(
             }
         }
 
-        context("여행일지 사용자 Id로 조회") {
+        context("여행 일지 사용자 Id로 조회") {
             expect("유저 ID와 일치하는 여행기록을 조회한다.") {
                 val result = travelJournalRepository.getByUserId(user.id)
+                result.size shouldBe 2
+            }
+        }
+
+        context("여행 일지 목록 조회") {
+            expect("나의 여행 일지 목록을 조회한다.") {
+                val result = travelJournalRepository.getMyTravelJournalSliceBy(
+                    userId = user.id,
+                    size = 10,
+                    lastId = null,
+                    sortType = TravelJournalSortType.LATEST,
+                )
+                result.size shouldBe 2
+            }
+
+            expect("내 친구의 여행 일지 목록을 조회한다.") {
+                val result = travelJournalRepository.getFriendTravelJournalSliceBy(
+                    userId = user.id,
+                    size = 10,
+                    lastId = null,
+                    sortType = TravelJournalSortType.LATEST,
+                )
                 result.size shouldBe 1
+                result[0] shouldBe friendTravelJournal
+            }
+
+            expect("추천 여행 일지 목록을 조회한다.") {
+                val result = travelJournalRepository.getRecommendTravelJournalSliceBy(
+                    user = user,
+                    size = 10,
+                    lastId = null,
+                    sortType = TravelJournalSortType.LATEST,
+                )
+                result.size shouldBe 1
+                result[0] shouldBe friendTravelJournal
             }
         }
     },
